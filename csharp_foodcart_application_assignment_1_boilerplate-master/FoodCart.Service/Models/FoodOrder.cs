5f88bd5fffa1551d3fa9cabf123715dc95d35961
github_Manish <m.karn2011@gmail.com>
2020-07-05 21:03:49 +0530

    modal class properties added

--- csharp_foodcart_application_assignment_1_boilerplate-master/FoodCart.Service/Models/FoodOrder.cs
+++ csharp_foodcart_application_assignment_1_boilerplate-master/FoodCart.Service/Models/FoodOrder.cs
@@ -1,9 +1,16 @@
-﻿namespace FoodCart.Service.Models
+﻿using System;
+
+namespace FoodCart.Service.Models
 {
     //Identify the attributes for FoodOrder based on requirement and define the FoodOrder class fields
 
     public class FoodOrder
     {
-
+        public string customerName { get; set; }
+        public string foodItem { get; set; }
+        public DateTime orderDate { get; set; }
+        public float price { get; set; }
+        public int quantity { get; set; }
+        public string couponCode { get; set; }
     }
 }
