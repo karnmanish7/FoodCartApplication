1452a051dd2d0afdfd407b69d7ccd786917b84f6
Kapil Ghimire <kapil.ghimire@unilever.com>
2020-07-18 12:38:41 +0530

    Passed all the test cases

--- csharp_foodcart_application_assignment_1_boilerplate-master/FoodCart.Service/Models/FoodOrder.cs
+++ csharp_foodcart_application_assignment_1_boilerplate-master/FoodCart.Service/Models/FoodOrder.cs
@@ -1,9 +1,16 @@
-﻿namespace FoodCart.Service.Models
+﻿using System;
+
+namespace FoodCart.Service.Models
 {
     //Identify the attributes for FoodOrder based on requirement and define the FoodOrder class fields
 
     public class FoodOrder
     {
-
+        public int quantity { get; set; }
+        public string customerName { get; set; }
+        public string foodItem { get; set; }
+        public DateTime orderDate { get; set; }
+        public double price { get; set; }
+        public int? couponCode { get; set; }
     }
 }
